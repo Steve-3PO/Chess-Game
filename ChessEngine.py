e944d9ea5212c5747f2f02098859297a8549167b
Stephen Delves <stephen.delves@hotmail.co.uk>
2023-03-23 21:39:00 +0000

    minor changes to start

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,5 +1,3 @@
-import pygame as p
-
 def move_to_coord(move):
     x = ord(move[0]) - ord('a')
     y = int(move[1]) - 1
@@ -21,26 +19,4 @@ class GameState:
         self.whitemove = True
         self.movelog = []
     
-    def show_board(self):
-        for row in self.board:
-            for piece in row:
-                print(piece, end="")
-            print('')
-
-
-    IMGS = {}
-    def loadImages():
-        pieces = ["bP", "bR", "bN", "bB", "bK", "bQ", "wP", "wR" "wN" "wB" "wK" "wQ"]
-        for piece in pieces:
-        IMGS[piece] = p.image.load("images/" + piece ".png")
-
-    def move_piece(self, move):
-
-        
-
-        board[] = board[]
-        board[] = '.'
-        
-
-        
     
--- ChessMain.py
+++ ChessMain.py
@@ -1,5 +1,27 @@
 import ChessEngine
+import pygame as p
+
+width = height = 512
+dimensions = 8
+sq_size = height // dimensions
+max_fps = 15
+imgs = {}
+p.init()
 
 #game = ChessEngine.GameState()
 #game.show_board()
+
+
+def show_board(self):
+    for row in self.board:
+        for piece in row:
+            print(piece, end="")
+        print('')
+
+
+def loadImages():
+    pieces = ["bP", "bR", "bN", "bB", "bK", "bQ", "wP", "wR" "wN" "wB" "wK" "wQ"]
+    for piece in pieces:
+        imgs[piece] = p.transform.scale(p.image.load("images/" + piece ".png"), (sq_size, sq_size))
+
 
