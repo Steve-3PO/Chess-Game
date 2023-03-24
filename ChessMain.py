834a1def59360da9330b184eceebcd057e987d52
Stephen Delves <stephen.delves@hotmail.co.uk>
2023-03-24 19:47:15 +0000

    first board loaded

--- ChessMain.py
+++ ChessMain.py
@@ -8,20 +8,50 @@ max_fps = 15
 imgs = {}
 p.init()
 
-#game = ChessEngine.GameState()
-#game.show_board()
-
-
 def show_board(self):
     for row in self.board:
         for piece in row:
             print(piece, end="")
         print('')
 
-
 def loadImages():
-    pieces = ["bP", "bR", "bN", "bB", "bK", "bQ", "wP", "wR" "wN" "wB" "wK" "wQ"]
+    pieces = ["bP", "bR", "bN", "bB", "bK", "bQ", "wP", "wR", "wN", "wB", "wK", "wQ"]
     for piece in pieces:
-        imgs[piece] = p.transform.scale(p.image.load("images/" + piece ".png"), (sq_size, sq_size))
+        imgs[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (sq_size, sq_size))
 
 
+def main():
+    screen = p.display.set_mode((width, height))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    game = ChessEngine.GameState()
+    loadImages()
+    
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        clock.tick(max_fps)
+        p.display.flip()
+        drawGameState(screen, game)
+
+def drawGameState(screen, game):
+    drawBoard(screen)
+    drawPieces(screen, game.board)
+    
+    
+def drawBoard(screen):
+    colors = [p.Color('white'), p.Color('gray')]
+    for r in range(dimensions):
+        for c in range(dimensions):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*sq_size, r*sq_size, sq_size, sq_size))
+    
+    
+def drawPieces(screen, board):
+    pass
+    
+if __name__ == '__main__':
+    main()
+    
